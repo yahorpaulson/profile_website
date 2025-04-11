a517221969dc808d19e6aeac1b2e059b56679dc2
yahorpaulson <y.paulson@yahoo.com>
2025-04-11 12:14:18 +0200

    add: arrow in greet section is drawn

--- frontend/src/components/HomePageComponent.vue
+++ frontend/src/components/HomePageComponent.vue
@@ -39,6 +39,8 @@
     .about{
         margin-bottom: 20%;
     }
+
+    
     
     
     .footer {
--- frontend/src/components/HomePageComponents/GreetSection.vue
+++ frontend/src/components/HomePageComponents/GreetSection.vue
@@ -1,11 +1,103 @@
 <template>
-    <div>
-        <h1>Hello friend!</h1>
-        <p>My name is Yahor Paulson. Welcome!</p>
+    <div class="greet-container">
+      <h1>Hello friend!</h1>
+      <p>My name is Yahor Paulson. Welcome!</p>
+      <canvas ref="arrowCanvas" class="arrow-canvas"></canvas>
     </div>
 </template>
 
+<script setup lang="ts">
+    
+    import { h, onMounted, ref } from 'vue';
+
+    const arrowCanvas = ref<HTMLCanvasElement | null>(null);
+
+    let y = 0
+
+    onMounted(() => {
+        const canvas = arrowCanvas.value
+        if (!canvas) return
+
+        const ctx = canvas.getContext('2d')
+        if (!ctx) return
+
+        const ratio = window.devicePixelRatio || 1 //1.25
+        const width = canvas.clientWidth || 100
+        const height = canvas.clientHeight || 100
+        
+
+        
+        canvas.width = width * ratio
+        canvas.height = height * ratio
+        ctx.scale(ratio, ratio)
+
+        
+        const x = width/2
+
+        
+
+        function drawArrow() {
+            if (!ctx || !canvas) return
+
+            ctx.clearRect(0, 0, width, height)
+
+            ctx.beginPath()
+            ctx.lineWidth = 2;
+            ctx.fillStyle = 'white'
+
+
+            ctx.moveTo(x, y)
+            ctx.lineTo(x-30, y + 50)
+            ctx.lineTo(x-10, y + 50)
+            ctx.lineTo(x-10, y+70)
+            ctx.lineTo(x+10, y+70)
+            ctx.lineTo(x+10, y+50)
+            ctx.lineTo(x+30, y+50)
+            ctx.lineTo(x, y)
+            
+
+            
+           
+        
+            ctx.fill();
+            
+            ctx.closePath()
+
+            y -= 5
+            if (y < 0) y = 400
+
+            requestAnimationFrame(drawArrow)
+        }
+        drawArrow();
+
+
+
+    })
+
+
+</script>
+
+
+
 <style scoped>
+
+.greet-container {
+  position: relative;
+  text-align: center;
+  
+}
+
+.arrow-canvas {
+  position: absolute;
+  width: 100px;
+  height: 100px;
+  background: transparent;
+  left: 50%;
+  transform: translateX(-50%);
+  pointer-events: none;
+  opacity: 0.5;
+  top: 20%;
+}
 
 @keyframes slideInFromLeft {
     from {
